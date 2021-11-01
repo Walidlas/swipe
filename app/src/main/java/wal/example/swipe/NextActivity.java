79cd89a384295a8db4d99a06f16d9d1fc88df849
Your Name <walidsayar2018@gmail.com>
2021-11-01 20:23:50 +0200

    swipe

---
+++ app/src/main/java/wal/example/swipe/NextActivity.java
@@ -0,0 +1,14 @@
+package wal.example.swipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_page);
+    }
+}
